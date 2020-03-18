a38af0896b9a2d23de1d98daa56199c4f9a81703
Miguel Angel Garcia Ruiz <miagarciaru@unal.edu.co>
2020-03-18 15:12:59 -0500

    Funcion seno en sumatoria

---
+++ Class18-03-20/functionmysin.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+
+double mysin (double x);
+
+int main ()
+{
+  std::cout.precision(16);
+  std::cout.setf(std::ios::scientific);
+  double x=0.0, dx=0.1, term=1.0, suma=0.0, eps=1.0e-8;
+  for (int ii=1; x<=2*3.141592; x+dx)
+    {
+      std::cout<<sum
+    }
+}
+
+double mysin (double x)
+{
+  double suma=x;
+  double term=1.0;
+  double eps=1.0e-8;
+  
+  
+}
