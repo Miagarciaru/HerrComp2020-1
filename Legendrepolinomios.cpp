c9e0a4eb685d3e114c4c152e09f9dcc6bc60bc5a
Miguel Angel Garcia Ruiz <miagarciaru@unal.edu.co>
2020-03-25 13:41:13 -0500

    Polinomios nde Legendre con librerias

---
+++ Legendrepolinomios.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+
+int main ()
+{
+  double dx=0.1;
+  for double(x=0; x<=10; x+=dx)
+    {
+      std::cout << std::legendre(2, x) <<"\n";
+    }
+  return 0;
+}
